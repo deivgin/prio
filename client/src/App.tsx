6a48863d631965b63e31efeb644ce8d51650d914
Deividas Gineitis <deividasgineitis123@gmail.com>
2024-09-20 07:22:45 +0300

    Add tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
-import type { Component } from "solid-js";
+import { createEffect, createSignal, Show, type Component } from "solid-js";
 
 const App: Component = () => {
-  return <div>Hello World</div>;
+  const [count, setCount] = createSignal(0);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
+  createEffect(() => {
+    console.log(count());
+  });
+
+  return (
+    <div class="flex align-middle justify-center">
+      <span>{count()}</span>
+
+      <button onClick={increment}>Increment</button>
+
+      <Show when={count() > 5}>
+        <div>Count is greater than 5</div>
+      </Show>
+    </div>
+  );
 };
 
 export default App;
